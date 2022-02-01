d46f4b54bcf6b1f1032779e68fc33024a58b4021
RushiDonga <rushi.donga.04@gmail.com>
2022-02-01 14:25:49 +0530

    Call API

--- api.js
+++ api.js
@@ -4,28 +4,22 @@ const apiFinalURL = new URLSearchParams(apiReplacedURL)
 
 var api_access_token = apiFinalURL.get('access_token')
 
-var sendData = (userName,email, Subject, description)=>{
-
-    console.log(userName)
-    console.log(email)
-    console.log(Subject)
-    console.log(description)
-
+var sendData = (userName,email, subject, description)=>{
 
     fetch(
         'https://mahtd90zi6.execute-api.ap-south-1.amazonaws.com/prod/put-data', 
         {
             method: "POST", 
-            body: {
-                "name": "Rushi Donga",
-                "email": "rushi.donga.04@gmail.com",
-                "subject":"This is a test Subject",
-                "description":"This is a test description"
-            },
+            body: JSON.stringify({
+                "name": userName,
+                "email": email,
+                "subject":subject,
+                "description":description
+            }),
             headers: {
                 'Content-Type': 'application/json',
                 'authentication': api_access_token
-              }
+              },
         }
     )
         .then(response => response.json())
