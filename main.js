60867ea7782f8817eb65bf31352b89b4d649a5a6
RushiDonga <rushi.donga.04@gmail.com>
2022-02-05 16:06:16 +0530

    test

--- main.js
+++ main.js
@@ -27,13 +27,15 @@ cognitoidentityserviceprovider.getUser(params, function(err, data) {
     {
         console.log(data);
 
-        for(var i = 0; i < data.UserAttributes.length; i++)
-        {   
-            if(data.UserAttributes[i].Name == 'name')
-            {
-                UserName = data.UserAttributes[i].Value;
-            }
-        }
+        UserName = data.Username
+
+        // for(var i = 0; i < data.UserAttributes.length; i++)
+        // {   
+        //     if(data.UserAttributes[i].Name == 'name')
+        //     {
+        //         UserName = data.UserAttributes[i].Value;
+        //     }
+        // }
 
         for(var j = 0; j < data.UserAttributes.length; j++)
         {
